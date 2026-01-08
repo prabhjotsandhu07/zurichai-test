d5c45be3b4591d18d30d95fa691afacb2f6a6cf4
Gurmeet Sandhu <gurmeet990@gmail.com>
2026-01-07 21:33:00 -0800

    fix region in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -276,7 +276,7 @@ resource "aws_security_group" "eks_cluster" {
 resource "aws_subnet" "eks_subnet_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
-  availability_zone       = "us-west-2a"
+  availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
   tags                    = { Name = "EKSSubnet1" }
 }
@@ -284,7 +284,7 @@ resource "aws_subnet" "eks_subnet_1" {
 resource "aws_subnet" "eks_subnet_2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.3.0/24"
-  availability_zone       = "us-west-2b"
+  availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
   tags                    = { Name = "EKSSubnet2" }
 }
